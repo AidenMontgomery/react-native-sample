962a2324f3bc674864bd5cb9581c151d4f90c4fb
Aiden Montgomery <aiden@constructivecoding.com>
2016-06-07 13:35:37 +1000

    Added delayed permission request

--- index.ios.js
+++ index.ios.js
@@ -13,21 +13,54 @@ import {
   Text,
   View,
   PushNotificationIOS,
-  TouchableHighlight
+  TouchableHighlight,
+  AppState
 } from 'react-native';
 
 class reactnativelocalpushnotifications extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      notificationToPost: undefined,
+      checkedNotificationPermissions: false,
+      allowedToReceiveNotifications: false
+    };
+  }
+
   componentWillMount() {
     // Add listener for local notifications
     PushNotificationIOS.addEventListener('localNotification', this._onLocalNotification);
+    PushNotificationIOS.addEventListener('register', this._onPushNotificationRegistration.bind(this));
   }
 
   componentWillUnmount() {
     // Remove listener for local notifications
     PushNotificationIOS.removeEventListener('localNotification', this._onLocalNotification);
+    PushNotificationIOS.removeEventListener('register', this._onPushNotificationRegistration);
   }
 
-  _scheduleNotification(soundName, badge) {
+  _onPushNotificationRegistration(token) {
+    console.log('Registered for notifications', token);
+
+    this.setState({
+      checkedNotificationPermissions: true,
+      allowedToReceiveNotifications: true
+    });
+
+    if (this.state.notificationToPost) {
+      this._scheduleNotification(this.state.notificationToPost);
+    }
+  }
+
+  _requestNotificationPermissions(notification) {
+    this.setState({
+      notificationToPost: notification
+    });
+
+    PushNotificationIOS.requestPermissions();
+  }
+
+  _prepareNotification(soundName, badge) {
     let notification = {
       alertBody: "Hello From app",
       applicationIconBadgeNumber: badge,
@@ -35,7 +68,36 @@ class reactnativelocalpushnotifications extends Component {
       soundName: soundName
     };
 
+    if (this.state.checkedNotificationPermissions) {
+      if (this.state.allowedToReceiveNotifications) {
+        this._scheduleNotification(notification);
+      }
+    } else {
+      PushNotificationIOS.checkPermissions((permissions) => {
+        console.log(permissions);
+
+        if (permissions.alert) {
+          this.setState({
+            checkedNotificationPermissions: true,
+            allowedToReceiveNotifications: true
+          });
+
+          this._scheduleNotification(notification);
+        } else {
+          this.setState({
+            checkedNotificationPermissions: true,
+            allowedToReceiveNotifications: false
+          });
+
+          this._requestNotificationPermissions(notification);
+        }
+      });
+    }
+  }
+
+  _scheduleNotification(notification) {
     PushNotificationIOS.scheduleLocalNotification(notification);
+    this.setState({notificationToPost: undefined})
   }
 
   _onLocalNotification(notification) {
@@ -50,16 +112,16 @@ class reactnativelocalpushnotifications extends Component {
   }
 
   componentDidMount() {
-    PushNotificationIOS.requestPermissions();
+    // PushNotificationIOS.requestPermissions();
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableHighlight style={styles.button} onPress={() => this._scheduleNotification('', 5)}>
+        <TouchableHighlight style={styles.button} onPress={() => this._prepareNotification('', 5)}>
           <Text style={styles.buttonText}>Schedule Silent Notification</Text>
         </TouchableHighlight>
-        <TouchableHighlight style={styles.button} onPress={() => this._scheduleNotification('default', -1)}>
+        <TouchableHighlight style={styles.button} onPress={() => this._prepareNotification('default', -1)}>
           <Text style={styles.buttonText}>Schedule Normal Notification</Text>
         </TouchableHighlight>
       </View>
